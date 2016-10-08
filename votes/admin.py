1f92c5946e96bb7aad3e0f0c22695c873cd615bc
Georges Khaznadar <georgesk@debian.org>
2016-10-08 17:20:48 +0200

    ajouté des champs de recherchee t des filtres aux pages d'administration

--- votes/admin.py
+++ votes/admin.py
@@ -3,18 +3,34 @@ from django.contrib import admin
 from .models import Enseignant, Formation, Horaire, Etudiant, Cours, \
     Inscription, Ouverture
 
-admin.site.register(Enseignant)
-admin.site.register(Formation)
 admin.site.register(Horaire)
-admin.site.register(Inscription)
 admin.site.register(Ouverture)
 
+class FormationAdmin (admin.ModelAdmin):
+    list_filter = ("duree","public_designe")
+    
+admin.site.register(Formation, FormationAdmin)
+
+class InscriptionAdmin (admin.ModelAdmin):
+    search_fields = ['etudiant__nom','cours__enseignant__nom']
+    
+admin.site.register(Inscription, InscriptionAdmin)
+
 class EtudiantAdmin (admin.ModelAdmin):
+    search_fields = ['nom']
     list_display = ('nom', 'prenom', 'classe', 'uidNumber', 'uid')
+    list_filter = ("classe",)
 
 admin.site.register(Etudiant, EtudiantAdmin)
 
+class EnseignantAdmin(admin.ModelAdmin):
+    search_fields = ['nom']
+    
+admin.site.register(Enseignant, EnseignantAdmin)
+
 class CoursAdmin (admin.ModelAdmin):
+    list_filter = ("horaire","enseignant")
+    
     def get_form(self, request, obj=None, **kwargs):
         """
         Don't allow adding new Cours Enseignant or Horaire
