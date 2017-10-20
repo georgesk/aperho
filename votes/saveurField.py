684eae6a592283b1cb380aae507fda111d4da8b2
Georges Khaznadar <georgesk@debian.org>
2017-10-20 20:22:49 +0200

    suite de l'implémentation

--- votes/saveurField.py
+++ votes/saveurField.py
@@ -2,7 +2,7 @@
 Champ pour définir une « saveur » relative à une barrette
 """
 
-from collections import OrderedDict
+from django.db import models
 
 class Ventilation:
     """
@@ -53,7 +53,7 @@ class SaveurDict:
         """
         Le constructeur
         @param effectif l'effectif maximal
-        @param categories un SortedDict nom -> Ventilation
+        @param categories un dictionnaire nom -> Ventilation
         """
         self.effectif=effectif
         self.saveurs=saveurs
@@ -61,7 +61,7 @@ class SaveurDict:
         return
 
     def __str__(self):
-        l=["  %s: %s\n" %(s, self.saveurs[s]) for s in self.saveurs]
+        l=["  %s: %s\n" %(s, self.saveurs[s]) for s in sorted(self.saveurs)]
         return "Saveurdict:\n"+"".join(l)
     
     def isMixte(self):
@@ -94,14 +94,102 @@ class SaveurDict:
                     t += self.saveurs[s].decr()
         assert(t==self.effectif)
         return
-                    
 
+def parse_saveurDict(saveurDictString):
+    """
+    Désérialise un objet SaveursDict de 5 saveurs au maximum.
+
+    Deux caractères donnent l'effectif total, puis 5 fois de suite
+    on cherche 5 caractère de nom de saveur, puis un caractère 0/1
+    actif, et deux caractères pour le nombre.
+    """
+    effectif=int(saveurDictString[0:2])
+    saveurDict={}
+    for i in range(5):
+        nom=saveurDictString[2+8*i:7+8*i].strip()
+        if nom:
+            saveurDict[nom]=Ventilation(
+                "0" != saveurDictString[7+8*i:8+8*i],
+                int(saveurDictString[8+8*i:10+8*i])
+            )
+    return SaveurDict(effectif,saveurDict)
+    
+    
+
+class SaveurDictField(models.Field):
+    description = "Un dictionnaire de saveurs (pas plus de cinq) pour l'AP"
+
+    def __init__(self, saveurDict=None, *args, **kwargs):
+        """
+        Le constructeur
+        @param saveurDict un objet SaveurDict (None par défaut)
+        """
+        # max 5 saveurs, sur 8 caractères chacune,
+        # plus 2 caractères d'effectif tital
+        kwargs['max_length'] = 42
+        self.saveurDict=saveurDict
+        super(SaveurDictField, self).__init__(*args, **kwargs)
+        return
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(SaveurDictField, self).deconstruct()
+        del kwargs["max_length"]
+        if self.saveurDict != None:
+            kwargs['saveurDict'] = self.saveurDict
+        return name, path, args, kwargs
+    
+    def from_db_value(self, value, expression, connection, context):
+        if value is None:
+            return value
+        return parse_saveurDict(value)
+
+    def to_python(self, value):
+        if isinstance(value, SaveurDict):
+            return value
+        if value is None:
+            return value
+        return parse_saveurDict(value)
+
+    def get_prep_value(self, saveurDict):
+        if not 0 <= saveurDict.effectif <= 99:
+            raise ValidationError("Effectif total incorrect : %s" % saveurDict.effectif)
+        result="%02d" % saveurDict.effectif
+        saveurs=sorted(saveurDict.keys())
+        savList=[]
+        for i in range(5):
+            sav=""
+            if i < len(saveurs):
+                nom=saveurs[i]
+                if len(nom) > 5:
+                    raise ValidationError("nom de saveur trop long : %s" % nom)
+                sav="%5s" % nom
+                if saveurDict[nom].actif:
+                    sav+="1"
+                else:
+                    sav+="0"
+                nombre=saveurDict[nom].nombre
+                if not 0 <= nombre <= 99:
+                    raise ValidationError("Effectif ventilé incorrect : %s" % nombre)
+                sav+="%02d" % nombre
+            else: # plus de saveurs, mais on doit aller jusqu'à 5
+                sav=" "*8
+            savList.append(sav)
+        # fin de la boucle de cinq
+        result+="".join(savList)
+                    
+        return result
+    
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        return self.get_prep_value(value)
+  
+    
 if __name__=="__main__":
-    sd=SaveurDict(18,OrderedDict([
-        ("tes",Ventilation(True,0)),
-        ("tl",Ventilation(True, 0)),
-        ("ts",Ventilation(False,0)),
-        ("tstmg",Ventilation(False,0)),
-    ]))
+    sd=SaveurDict(18,{
+        "tes":   Ventilation(True,0),
+        "tl":    Ventilation(True, 0),
+        "ts":    Ventilation(False,0),
+        "tstmg": Ventilation(False,0),
+    })
     print(sd)
     
