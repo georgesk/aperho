d1c094c910c967a80396bd16ff6ecd7e28adbac8
Georges Khaznadar <georgesk@debian.org>
2020-01-16 20:13:31 +0100

    changed the way to import reverse

--- aperho/home.py
+++ aperho/home.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
