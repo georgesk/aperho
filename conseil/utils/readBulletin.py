641c60e7ffc7cf7cf03953276b6b27387e99b9e8
Georges Khaznadar <georgesk@georges-pc>
2021-07-30 11:14:53 +0200

    found some bugs; better CSS styling

--- conseil/utils/readBulletin.py
+++ conseil/utils/readBulletin.py
@@ -11,7 +11,10 @@ def _visite_fichier(csvfile, head=None, body=None, foot=None):
     @return une chaîne composé de la concaténation des résultats de
             head, body, foot
     """
-    r = csv.DictReader(csvfile, delimiter=";")
+    h1 = csvfile.readline().split(";")
+    h2 = csvfile.readline().split(";")
+    h = h2[:3] + h1[3:]
+    r = csv.DictReader(csvfile, fieldnames = h, delimiter=";")
     result = ""
     if head:
         result += head(r)
@@ -41,13 +44,25 @@ def visite_fichier(f, head=None, body=None, foot=None):
 
 if __name__ == "__main__":
     def head_table(r):
-        result = "<table class=\"matable\">\n<tr>"
+        result = """\
+<html>
+<head>
+<title>Test</title>
+<style>
+table {border-collapse: collapsed}
+th, td {border: 1px solid black; background: rgba(255,255,200,0.5);}
+tr:nth-child(odd) {background: lightcyan}
+</style>
+</head>
+<body>
+<table class="matable">\n<tr>
+"""
         for h in r.fieldnames:
             result += f"<th>{h}</th>"
         result += "</tr>\n"
         return result
     def foot_table(r):
-        return "</table>\n"
+        return "</table>\n</body>\n</html>\n"
     def body_table(row):
         result = "<tr>"
         for col, val in row.items():
